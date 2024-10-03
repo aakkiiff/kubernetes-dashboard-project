99837b30666c8859ccde7367c364c833e3e75a09
akif <jackakif@gmail.com>
2024-10-04 01:21:24 +0600

    app creation pod-->deployment

--- routes/app.go
+++ routes/app.go
@@ -2,9 +2,11 @@ package routes
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -17,12 +19,76 @@ import (
 func stringPtr(s string) *string {
 	return &s
 }
+func int32Ptr(i int32) *int32 { return &i }
+
+func updateApp(c *gin.Context) {
+	var appModel models.App
+	err := c.ShouldBindJSON(&appModel)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid request", "error": err.Error()})
+		return
+	}
+
+	NamespaceName := appModel.Name + "-ns"
+	clientset := configinit.Initialize_config()
+
+	_, err = clientset.CoreV1().Namespaces().Get(context.Background(), NamespaceName, v1.GetOptions{})
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "application do not exist! Please create an application first")
+		return
+	}
+
+	pod, err := clientset.CoreV1().Pods(NamespaceName).Get(context.Background(), appModel.Name, v1.GetOptions{})
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "pod do not exist! Someone might have deleted it! Please re-apply the application first")
+		return
+	}
+	service, err := clientset.CoreV1().Services(NamespaceName).Get(context.Background(), appModel.Name, v1.GetOptions{})
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "service do not exist! Someone might have deleted it! Please re-apply the application first")
+		return
+	}
+	_, err = clientset.NetworkingV1().Ingresses(NamespaceName).Get(context.Background(), appModel.Name+"-ingress", v1.GetOptions{})
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "ingress do not exist! Someone might have deleted it! Please re-apply the application first")
+		return
+	}
+	fmt.Println("new:", appModel.Image+" old:", pod.Spec.Containers[0].Image)
+	if appModel.Image != pod.Spec.Containers[0].Image {
+		pod.Spec.Containers[0].Image = appModel.Image
+
+		_, err = clientset.CoreV1().Pods(NamespaceName).Update(context.Background(), pod, v1.UpdateOptions{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "could not update the pod", "error": err.Error()})
+			return
+		}
+	}
+
+	if appModel.ContainerPort != int(pod.Spec.Containers[0].Ports[0].ContainerPort) {
+		// fmt.Println(pod.Spec.Containers[0].Ports[0].ContainerPort," ", int32(appModel.ContainerPort))
+		pod.Spec.Containers[0].Ports[0].ContainerPort = int32(appModel.ContainerPort)
+		service.Spec.Ports[0].Port = int32(appModel.ContainerPort)
+		_, err = clientset.CoreV1().Pods(NamespaceName).Update(context.Background(), pod, v1.UpdateOptions{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "could not update the pod", "error": err.Error()})
+			return
+		}
+		// _, err = clientset.CoreV1().Services(NamespaceName).Update(context.Background(), service, v1.UpdateOptions{})
+		// if err != nil {
+		// 	c.JSON(http.StatusInternalServerError, gin.H{"message": "could not update the service", "error": err.Error()})
+		// 	return
+		// }
+	}
+
+}
 
 func createApp(c *gin.Context) {
 	var appModel models.App
 
 	namespaceCreationStatus := "namespace created"
-	podCreationStatus := "pod created"
+	// podCreationStatus := "pod created"
+	deploymentCreationStatus := "deployment created"
 	serviceCreationStatus := "service created"
 	ingressCreationStatus := "ingress created"
 
@@ -39,19 +105,62 @@ func createApp(c *gin.Context) {
 		},
 	}
 
-	pod := &corev1.Pod{
+	// pod := &corev1.Pod{
+	// 	ObjectMeta: v1.ObjectMeta{
+	// 		Name:      appModel.Name,
+	// 		Namespace: NamespaceName,
+	// 		Labels: map[string]string{
+	// 			"podName": appModel.Name,
+	// 		},
+	// 	},
+	// 	Spec: corev1.PodSpec{
+	// 		Containers: []corev1.Container{
+	// 			{
+	// 				Name:  appModel.Name + "-container",
+	// 				Image: appModel.Image,
+	// 				Ports: []corev1.ContainerPort{
+	// 					{
+	// 						ContainerPort: int32(appModel.ContainerPort),
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }
+
+	deployment := &appsv1.Deployment{
 		ObjectMeta: v1.ObjectMeta{
 			Name:      appModel.Name,
 			Namespace: NamespaceName,
 			Labels: map[string]string{
-				"podName": appModel.Name,
+				"app": appModel.Name,
 			},
 		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:  appModel.Name + "-container",
-					Image: appModel.Image,
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(1),
+			Selector: &v1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": appModel.Name,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: v1.ObjectMeta{
+					Labels: map[string]string{
+						"app": appModel.Name,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  appModel.Name + "-container",
+							Image: appModel.Image,
+							Ports: []corev1.ContainerPort{
+								{
+									ContainerPort: int32(appModel.ContainerPort),
+								},
+							},
+						},
+					},
 				},
 			},
 		},
@@ -64,13 +173,13 @@ func createApp(c *gin.Context) {
 		},
 		Spec: corev1.ServiceSpec{
 			Selector: map[string]string{
-				"podName": appModel.Name,
+				"app": appModel.Name,
 			},
 			Ports: []corev1.ServicePort{
 				{
 					Name:       "http",
 					Port:       80,
-					TargetPort: intstr.FromInt(80),
+					TargetPort: intstr.FromInt(appModel.ContainerPort),
 					Protocol:   corev1.ProtocolTCP,
 				},
 			},
@@ -122,7 +231,7 @@ func createApp(c *gin.Context) {
 	if err != nil {
 		if errors.IsAlreadyExists(err) {
 			// c.JSON(http.StatusInternalServerError, gin.H{"message": "namspace already exists, app will not be created", "error": err.Error()})
-			namespaceCreationStatus = "pod already exists, will not be recreated"
+			namespaceCreationStatus = "namespace already exists, will not be recreated"
 
 		} else {
 			c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to create namespace", "error": err.Error()})
@@ -130,17 +239,28 @@ func createApp(c *gin.Context) {
 		}
 
 	}
-	CheckNamespaceExists(NamespaceName)
-	_, err = clientset.CoreV1().Pods(NamespaceName).Create(context.Background(), pod, v1.CreateOptions{})
+	CheckNamespaceReady(NamespaceName)
+
+	// _, err = clientset.CoreV1().Pods(NamespaceName).Create(context.Background(), pod, v1.CreateOptions{})
+	// if err != nil {
+	// 	if errors.IsAlreadyExists(err) {
+	// 		podCreationStatus = "pod already exists, will not be recreated"
+
+	// 	} else {
+	// 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to create pod", "error": err.Error()})
+	// 		return
+	// 	}
+
+	// }
+
+	_, err = clientset.AppsV1().Deployments(NamespaceName).Create(context.Background(), deployment, v1.CreateOptions{})
 	if err != nil {
 		if errors.IsAlreadyExists(err) {
-			podCreationStatus = "pod already exists, will not be recreated"
-
+			deploymentCreationStatus = "Deployment already exists, will not be recreated"
 		} else {
-			c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to create pod", "error": err.Error()})
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to create deployment", "error": err.Error()})
 			return
 		}
-
 	}
 
 	_, err = clientset.CoreV1().Services(NamespaceName).Create(context.Background(), service, v1.CreateOptions{})
@@ -166,17 +286,17 @@ func createApp(c *gin.Context) {
 	}
 	c.JSON(http.StatusCreated, gin.H{
 		"Status": map[string]string{
-			"namespace": namespaceCreationStatus,
-			"pod":       podCreationStatus,
-			"service":   serviceCreationStatus,
-			"ingress":   ingressCreationStatus,
+			"namespace":  namespaceCreationStatus,
+			"deployment": deploymentCreationStatus,
+			"service":    serviceCreationStatus,
+			"ingress":    ingressCreationStatus,
 		},
 		"Application": map[string]string{
-			"pod_name":       appModel.Name,
-			"namespace":      NamespaceName,
-			"containerImage": appModel.Image,
-			"service_name":   appModel.Name,
-			"ingress":        appModel.Name + "-ingress",
+			"deployment_name": appModel.Name,
+			"namespace":       NamespaceName,
+			"containerImage":  appModel.Image,
+			"service_name":    appModel.Name,
+			"ingress":         appModel.Name + "-ingress",
 		},
 	})
 }
